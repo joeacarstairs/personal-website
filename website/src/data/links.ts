31e75134fefeba2e7e8db5a4b23f6c309642f9db
Joe Carstairs <65492573+Sycamost@users.noreply.github.com>
2024-06-05 08:00:32 +0100

    Another link

--- website/src/data/links.ts
+++ website/src/data/links.ts
@@ -14,6 +14,12 @@ const links: Link[] = [
     description: 'ChatGPT 3.5 gives bad answers half the time, and programmers miss the mistakes almost half the time. Be careful out there, folks.',
     isoDateAdded: '2024-06-04',
   },
+  {
+    href: 'https://www.colinmcginn.net/primary-and-secondary-values',
+    title: 'Primary and Secondary Values',
+    description: 'Colin McGinn makes the case for primary and secondary moral values, just as there are primary and secondary qualities, apparently thereby managing to assert both moral realism and anti-realism at the same time without contradiction.',
+    isoDateAdded: '2024-06-05',
+  },
 ];
 
 export default links;
