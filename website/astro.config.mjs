e5cc05519a7736f9bd4554366fcd199b23d88566
Joe Carstairs <me@joeac.net>
2025-12-12 14:13:16 +0000

    node.js adapter

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import node from "@astrojs/node";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://joeac.net',
-	integrations: [mdx(), sitemap()],
+  adapter: node({
+    mode: "standalone",
+  }),
+  site: "https://joeac.net",
+  integrations: [mdx(), sitemap()],
 });
