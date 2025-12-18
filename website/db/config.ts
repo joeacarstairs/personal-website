a85b7b36c6b549642c8cc125a498fa521588bf5e
Joe Carstairs <me@joeac.net>
2025-12-18 10:59:15 +0000

    adds SentEmails and SendmailToken tables to DB

--- website/db/config.ts
+++ website/db/config.ts
@@ -1,5 +1,34 @@
-import { defineDb } from "astro:db";
+import { column, defineDb, defineTable } from "astro:db";
+
+const Otp = defineTable({
+  columns: {
+    userId: column.text(),
+    value: column.text(),
+    createdAt: column.number(),
+    validUntil: column.number(),
+  },
+});
+
+const SendmailToken = defineTable({
+  columns: {
+    userId: column.text(),
+    value: column.text(),
+    createdAt: column.number(),
+    validUntil: column.number(),
+  },
+});
+
+const SentEmails = defineTable({
+  columns: {
+    messageId: column.text(),
+    sentAt: column.number(),
+  },
+});
 
 export default defineDb({
-  tables: {},
+  tables: {
+    Otp,
+    SendmailToken,
+    SentEmails,
+  },
 });
