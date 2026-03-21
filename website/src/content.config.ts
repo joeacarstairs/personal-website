9332601f5b94ca980c76ed604a88ad013bf99b22
Joe Carstairs <me@joeac.net>
2026-03-21 22:22:42 +0000

    upgrade to Astro 6

--- website/src/content.config.ts
+++ website/src/content.config.ts
@@ -1,15 +1,19 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.(md|mdx|html)', base: './src/content/blog' }),
+  loader: glob({
+    pattern: "**/*.(md|mdx|html)",
+    base: "./src/content/blog",
+  }),
   schema: z.object({
-		title: z.string(),
-		hidden: z.optional(z.boolean()),
-		description: z.string(),
-		pubDate: z.date(),
-		updatedDate: z.optional(z.date()),
-	}),
+    title: z.string(),
+    hidden: z.optional(z.boolean()),
+    description: z.string(),
+    pubDate: z.date(),
+    updatedDate: z.optional(z.date()),
+  }),
 });
 
 export const collections = { blog };
