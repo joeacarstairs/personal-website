a575ecdd681070bb77c0106332793f44b8457066
Joe Carstairs <me@joeac.net>
2026-06-25 11:30:55 +0100

    don't make anything in gemini/run.sh

--- gemini/run.sh
+++ gemini/run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-make && \
-  ( ( sleep 5; comitium refresh --data comitium-data/ ) & ) \
+( ( sleep 5; comitium refresh --data comitium-data/ ) & ) \
   && agate --content content/ --addr [::]:1965 --addr 0.0.0.0:1965 --lang en-GB
