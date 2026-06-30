5aa03f88dcade867fa0cab5bc6ac0b900a4b6c01
Joe Carstairs <me@joeac.net>
2026-06-30 09:46:17 +0100

    fixes ln/install.mk

--- ln/install.mk
+++ ln/install.mk
@@ -5,8 +5,8 @@ install: /var/ln.joeac.net/public
 
 /var/ln.joeac.net/public: $(LN_DIR)/public
 	sudo rm -rf /var/ln.joeac.net/public
-	sudo mkdir -p /var/ln.joeac.net/public
-	sudo cp -r $(LN_DIR)/public
+	sudo mkdir -p /var/ln.joeac.net/
+	sudo cp -r $(LN_DIR)/public/ /var/ln.joeac.net/public/
 
 .PHONY: uninstall
 uninstall:
