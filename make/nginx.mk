cfa0dd5a9a5df70a56d5d1eb04ce3b59fe65a189
Joe Carstairs <me@joeac.net>
2026-07-01 18:39:20 +0100

    adds reinstall/uninstall nginx module rules

--- make/nginx.mk
+++ make/nginx.mk
@@ -14,6 +14,8 @@ uninstall_nginx_$(module):
 endef
 
 $(foreach module,$(MODULES),$(eval $(install_nginx_module_rule)))
+$(foreach module,$(MODULES),$(eval $(reinstall_nginx_module_rule)))
+$(foreach module,$(MODULES),$(eval $(uninstall_nginx_module_rule)))
 
 /etc/nginx/http.d/%.joeac.net.conf: nginx/http.d/%.joeac.net.conf /etc/nginx/http.d /etc/nginx/nginx.conf
 	sudo cp $< $@
