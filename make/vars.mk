c89f338601cd324a24b21a8a2b5bb4666779bba8
Joe Carstairs <me@joeac.net>
2026-07-02 17:54:18 +0100

    re-orders vars.mk

--- make/vars.mk
+++ make/vars.mk
@@ -1,3 +1,17 @@
+HOSTNAME := $(shell cat /etc/hostname)
+HOSTNAMES := pi-broughton blade-canongate
+MASTER_NODE := pi-broughton
+MODULES_pi-broughton := http gemini smtp vaultwarden ln
+MODULES_blade-canongate := etherpad
+MODULES := $(MODULES_$(HOSTNAME))
+ALL_MODULES := $(sort $(foreach hostname,$(HOSTNAMES),$(MODULES_$(hostname))))
+IS_MASTER_NODE := $(filter $(MASTER_NODE),$(HOSTNAME))
+NGINX_MODULES := $(if $(IS_MASTER_NODE),$(ALL_MODULES))
+COMPOSE_SERVICES := $(shell podman-compose config \
+	| yq ".services | keys" --output-format csv --csv-separator " ")
+MAKE_MODULES := $(foreach module,$(MODULES),\
+	$(shell [ -f $(module)/Makefile ] && echo $(module)))
+
 SUBDOMAIN_http := @
 SUBDOMAIN_vaultwarden := pwd
 SUBDOMAIN_etherpad := docs
@@ -9,21 +23,8 @@ PORT_gemini := 1965
 PORT_smtp := 2500
 PORT_vaultwarden := 9000
 
-HOSTNAME := $(shell cat /etc/hostname)
-HOSTNAMES := pi-broughton blade-canongate
-MODULES_pi-broughton := http gemini smtp vaultwarden ln
-MODULES_blade-canongate := etherpad
-ALL_MODULES := $(sort $(foreach hostname,$(HOSTNAMES),$(MODULES_$(hostname))))
-MASTER_NODE := pi-broughton
-IS_MASTER_NODE := $(filter $(MASTER_NODE),$(HOSTNAME))
-NGINX_MODULES := $(if $(IS_MASTER_NODE),$(ALL_MODULES))
-MODULES := $(MODULES_$(HOSTNAME))
 SUBDOMAINS := $(foreach module,$(MODULES),$(SUBDOMAIN_$(module)))
 NGINX_SUBDOMAINS := $(foreach module,$(NGINX_MODULES),$(SUBDOMAIN_$(module)))
-COMPOSE_SERVICES := $(shell podman-compose config \
-	| yq ".services | keys" --output-format csv --csv-separator " ")
-MAKE_MODULES := $(foreach module,$(MODULES),\
-	$(shell [ -f $(module)/Makefile ] && echo $(module)))
 
 ENV_RULES := $(foreach module,$(MODULES),$(module)/.env)
 MAKE_RULES := $(foreach module,$(MAKE_MODULES),make_$(module))
