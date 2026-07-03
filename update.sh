3af454683946a00772979a5dc88b53311be05612
Joe Carstairs <me@joeac.net>
2026-07-03 13:38:21 +0100

    adds XDG_RUNTIME_DIR explicitly in update.sh

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 cd /home/joeac.net/joeac.net \
   && sudo -u joeac.net git pull \
   && sudo -u joeac.net make \
-  && sudo -u joeac.net make reinstall
+  && sudo -u joeac.net XDG_RUNTIME_DIR=/tmp/xdg/$(id -u joeac.net)-xdg-runtime-dir make reinstall
